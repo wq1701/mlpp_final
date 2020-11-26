72f7b784a45d203f823c5f3cbc959668e5cc7866
weiwei_qi <qww980105@gmail.com>
2020-11-25 19:44:04 -0500

    add Poisions and update Gaussian

--- final-project/PoissonMF.py
+++ final-project/PoissonMF.py
@@ -1,6 +1,6 @@
-# Gaussian Prior
+# Poisson prior
 
-def run_GaussianMF(anime_matrix_train, anime_data_test, k, method = "svi", lr=0.05, n_steps = 1000, mae_tol = 0.02): 
+def run_PoissonMF(anime_matrix_train, anime_data_test, k, method = "svi", lr=0.05, n_steps = 1000, mae_tol = 0.02): 
     import logging
     import os
     import warnings
@@ -36,7 +36,7 @@ def run_GaussianMF(anime_matrix_train, anime_data_test, k, method = "svi", lr=0.
     sigma_u = torch.tensor(1.0)
     sigma_v = torch.tensor(1.0)
     
-    def matrix_factorization_normal(anime_matrix_train, k = k):
+    def matrix_factorization_poisson(anime_matrix_train, k = k):
         
         m = anime_matrix_train.shape[0]
         n = anime_matrix_train.shape[1]
@@ -51,75 +51,34 @@ def run_GaussianMF(anime_matrix_train, anime_data_test, k, method = "svi", lr=0.
         v = pyro.sample("v", dist.Normal(loc = v_mean, scale = v_sigma).to_event(2))
         
         expectation = torch.mm(u, v.t())
-        sigma = pyro.sample("sigma", dist.Uniform(0., 10.))
+        expectation[expectation <= 0] = 0.01 # softly make all values positive for poisson
         is_observed = (~np.isnan(anime_matrix_train))
         is_observed = torch.tensor(is_observed)
         valid_matrix = torch.tensor(anime_matrix_train).clone()
         valid_matrix[~is_observed] = 0  # ensure all values are valid
+        valid_matrix = np.around(valid_matrix) # round all observed values to positive integers
         
         with pyro.plate("user", m, dim=-2): 
             with pyro.plate("anime", n, dim=-3):
                 with pyro.poutine.mask(mask=is_observed):
-                    pyro.sample("obs", dist.Normal(expectation, sigma), 
+                    pyro.sample("obs", dist.Poisson(expectation), 
                             obs = valid_matrix)
 
-    def guide_map(anime_matrix_train, k = k):
-        m = anime_matrix_train.shape[0]
-        n = anime_matrix_train.shape[1]
-        
-        u_map = pyro.param('u_map', torch.zeros([m, k]))
-        v_map = pyro.param('v_map', torch.zeros([n, k]))
-        sigma_map = pyro.param("sigma_map", torch.tensor(1.0), constraint=constraints.positive)
-        
-        pyro.sample("u", dist.Delta(u_map).to_event(2))
-        pyro.sample("v", dist.Delta(v_map).to_event(2))
-        pyro.sample("sigma", dist.Delta(sigma_map))
-        
+    def guide_svi_pois(anime_matrix_train, k = k):
     
-    def guide_svi(anime_matrix_train, k = k):
         m = anime_matrix_train.shape[0]
         n = anime_matrix_train.shape[1]
-
+        
         u_mean = pyro.param('u_mean', torch.zeros([m, k]))
         u_sigma = pyro.param('u_sigma', torch.ones([m, k]) * sigma_u, constraint=constraints.positive)
 
         v_mean = pyro.param('v_mean', torch.zeros([n, k]))
         v_sigma = pyro.param('v_sigma', torch.ones([n, k]) * sigma_v, constraint=constraints.positive)
-
-        sigma_loc = pyro.param('sigma_loc', torch.tensor(1.),
-                                 constraint=constraints.positive)
-        sigma_scale = pyro.param('sigma_scale', torch.tensor(0.1),
-                                 constraint=constraints.positive)
-
+        
         pyro.sample("u", dist.Normal(u_mean, u_sigma).to_event(2))
         pyro.sample("v", dist.Normal(v_mean, v_sigma).to_event(2))
-        pyro.sample("sigma", dist.Normal(sigma_loc, sigma_scale))
-        
     
-    def train_via_opt_map(model, guide):
-        pyro.clear_param_store()
-        svi = SVI(model, guide, optim.Adam({"lr": lr}), loss=Trace_ELBO())
-        
-        loss_list = []
-        mae_list = []
-        for step in range(n_steps):
-            loss = svi.step(anime_matrix_train.values)
-            pred = []
-            for i,j in anime_data_test[["user_id", "anime_id"]].itertuples(index=False):
-                r = torch.dot(pyro.param("u_map")[i-1, :], pyro.param("v_map")[j-1, :])
-                r = r.item()
-                pred.append(r)
-            MAE = mae(anime_data_test.rating, pred)
-            if step > 500 and MAE - min(mae_list) > mae_tol:
-                print('[stop at iter {}]  loss: {:.4f} Test MAE: {:.4f}'.format(step, loss, MAE))
-                break
-            loss_list.append(loss)
-            mae_list.append(MAE)
-            if step % 250 == 0:
-                print('[iter {}]  loss: {:.4f} Test MAE: {:.4f}'.format(step, loss, MAE))
-        return(loss_list, mae_list)
-    
-    def train_via_opt_svi(model, guide):
+    def train_via_opt(model, guide):
         pyro.clear_param_store()
         svi = SVI(model, guide, optim.Adam({"lr": lr}), loss=Trace_ELBO())
 
@@ -133,18 +92,19 @@ def run_GaussianMF(anime_matrix_train, anime_data_test, k, method = "svi", lr=0.
                 r = r.item()
                 pred.append(r)
             MAE = mae(anime_data_test.rating, pred)
-            if step > 500 and MAE - min(mae_list) > mae_tol:
+            if step > 100 and MAE - min(mae_list) > mae_tol:
                 print('[stop at iter {}]  loss: {:.4f} Test MAE: {:.4f}'.format(step, loss, MAE))
                 break
             loss_list.append(loss)
             mae_list.append(MAE)
             if step % 250 == 0:
                 print('[iter {}]  loss: {:.4f} Test MAE: {:.4f}'.format(step, loss, MAE))
-        return(loss_list, mae_list)
+        return(loss_list, mae_list) 
     
-    if method == "map":
-        loss_list, mae_list = train_via_opt_map(matrix_factorization_normal, guide_map)
+    # if method == "map":
+    #     loss_list, mae_list = train_via_opt_map(matrix_factorization_normal, guide_map)
     if method == "svi":
         loss_list, mae_list = train_via_opt_svi(matrix_factorization_normal, guide_svi)
 
     return loss_list, mae_list
+
