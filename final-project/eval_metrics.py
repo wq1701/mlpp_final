ab4e86ab4c94cb1788eb3d8982021da020d69954
weiwei_qi <qww980105@gmail.com>
2020-11-29 19:03:27 -0500

    pass flake8

--- final-project/eval_metrics.py
+++ final-project/eval_metrics.py
@@ -1,9 +1,11 @@
 import numpy as np
 
+
 def rmse(true, pred):
     true = np.array(true)
     pred = np.array(pred)
     return np.sqrt(np.mean((true - pred)**2))
+
 
 def mae(true, pred):
     true = np.array(true)
