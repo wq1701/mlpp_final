eeed3e1962a252d328c7272fc3fb4d9cc3590e18
weiwei_qi <qww980105@gmail.com>
2020-11-29 19:36:48 -0500

    pass flake 8

--- final-project/utils.py
+++ final-project/utils.py
@@ -1,20 +1,19 @@
-def load_makematrix(path_in_str, split = False, test_ratio = 0.1, random_seed = 42): 
-    import numpy as np
+def load_makematrix(path_in_str, split=False,
+                    test_ratio=0.1, random_seed=42):
     import pandas as pd
     from sklearn.model_selection import train_test_split
-    
-    data_full = pd.read_csv(path_in_str)
-    
-    
-    if split == True: 
-        data_train, data_test = train_test_split(data_full, test_size = test_ratio, random_state = random_seed)
-        data_train_matrix = data_train.pivot(index='user_id', columns='anime_id', values='rating')
-    
-        return data_full, data_train_matrix, data_test
-    
-    else:
-        data_matrix = data_full.pivot(index='user_id', columns='anime_id', values='rating')
-        return data_full, data_matrix
-    
-    
 
+    data_full = pd.read_csv(path_in_str)
+
+    if split is True:
+        data_train, data_test = train_test_split(
+            data_full, test_size=test_ratio, random_state=random_seed)
+        data_train_matrix = data_train.pivot(
+            index='user_id', columns='anime_id', values='rating')
+
+        return data_full, data_train_matrix, data_test
+
+    else:
+        data_matrix = data_full.pivot(
+            index='user_id', columns='anime_id', values='rating')
+        return data_full, data_matrix
